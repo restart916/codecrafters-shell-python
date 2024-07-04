9767bfac729985124145f224443879134c61e91a
Yongsang Lee <restart916@gmail.com>
2024-07-04 18:02:08 +0900

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -7,12 +7,17 @@ def main():
         sys.stdout.write("$ ")
         sys.stdout.flush()
 
-        command = input()
-        if command == "exit 0":
-            return 0
-
-        sys.stdout.write(f'{command}: command not found\n')
-        sys.stdout.flush()
+        input_command = input()
+        command = input_command.split(" ")[0]
+        args = input_command.split(" ")[1:]
+        if command == "exit":
+            return args[0]
+        elif command == "echo":
+            sys.stdout.write(" ".join(args) + "\n")
+            sys.stdout.flush()
+        else:
+            sys.stdout.write(f'{command}: command not found\n')
+            sys.stdout.flush()
 
 
 if __name__ == "__main__":
