c64e73e54d586c63b5ca184e5882b35ede4b4108
Yongsang Lee <restart916@gmail.com>
2024-07-05 12:33:47 +0900

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,6 +1,26 @@
 import sys
 
 
+commands = {
+    "echo": {
+        "func": lambda args: " ".join(args),
+    },
+    "exit": {
+        "func": lambda args: args[0],
+    },
+    "type": {
+        "func": lambda args: type_command(args),
+    }
+}
+
+def type_command(args):
+    command = args[0]
+    if command in commands:
+        return f"{command} is a shell builtin"
+    else:
+        return f"{command}: not found"
+    
+
 def main():
     # Wait for user input
     while True:
@@ -10,10 +30,12 @@ def main():
         input_command = input()
         command = input_command.split(" ")[0]
         args = input_command.split(" ")[1:]
+
         if command == "exit":
             return args[0]
-        elif command == "echo":
-            sys.stdout.write(" ".join(args) + "\n")
+        elif command in commands:
+            result = commands[command]["func"](args)
+            sys.stdout.write(result + "\n")
             sys.stdout.flush()
         else:
             sys.stdout.write(f'{command}: command not found\n')
